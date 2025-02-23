747a5ac60bfd6b686321da49b49d16b6ac08553d
Ashish Agarwal <code.with.aasheesh@gmail.com>
2025-02-23 18:23:40 +0530

    fix a bug

--- components/entryform.tsx
+++ components/entryform.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { useState } from "react";
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { createEntry } from "@/lib/db";
